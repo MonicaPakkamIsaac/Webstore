eafcd58bc22e191c7a341ba1e91b6c4ffec38fb5
rajocular <raja.ms96@gmail.com>
2018-07-04 18:35:59 -0400

    category model is created

--- storeapp/models.py
+++ storeapp/models.py
@@ -4,3 +4,11 @@ from django.utils import timezone
 
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    warehouse = models.CharField(max_length=100, null=False, blank=False, default=' ')
+
+    def __str__(self):
+        return self.name
