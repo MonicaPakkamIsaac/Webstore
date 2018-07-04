061c75573018d7b2bb641a3f9b531c2b973c272d
rajocular <raja.ms96@gmail.com>
2018-07-04 18:58:03 -0400

    InterestForm is created

--- storeapp/forms.py
+++ storeapp/forms.py
@@ -7,3 +7,8 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('product', 'num_units', )
         labels = {'num_units': 'Quantity'}
+
+
+class InterestForm(forms.Form):
+    interested = forms.IntegerField(widget=forms.RadioSelect(choices=[('1', 'Yes'), ('0', 'No')]), required=True, initial=1)
+    comments = forms.CharField(widget=forms.Textarea(), label="Additional Comments", required=False)
