1b3db56a3ca6600cce4346c37ccf4023b76d9046
rajocular <raja.ms96@gmail.com>
2018-07-04 18:48:16 -0400

    index view is created

--- storeapp/views.py
+++ storeapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import get_object_or_404, render, redirect
 import datetime
 
 # Create your views here.
+
+
+def index(request):
+    cat_list = Category.objects.all().order_by('id')[:10]
+    pro_list = Product.objects.all().order_by('-price')[:5]
+    return render(request, 'storeapp/index.html', {'cat_list': cat_list, 'pro_list': pro_list, 'msg': msg})
