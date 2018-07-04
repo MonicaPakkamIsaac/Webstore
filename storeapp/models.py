3cf160a39a0d9352b36603cd75cf21b2b35c05bc
rajocular <raja.ms96@gmail.com>
2018-07-04 18:38:09 -0400

    product model is created

--- storeapp/models.py
+++ storeapp/models.py
@@ -12,3 +12,24 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=100, validators={})
+    available = models.BooleanField(default=True)
+    description = models.TextField(max_length=200, null=True, blank=True)
+    interested = models.PositiveIntegerField(default=0)
+
+    def refill(self, num=0):
+        if num:
+            self.stock = self.stock + num
+            self.save()
+        else:
+            self.stock = self.stock + 100
+            self.save()
+
+    def __str__(self):
+        return self.name
