00fac001cea30f0a914ff744ba6bc257db353730
rajocular <raja.ms96@gmail.com>
2018-07-04 19:48:15 -0400

    index.html update

--- storeapp/views.py
+++ storeapp/views.py
@@ -13,7 +13,7 @@ import datetime
 def index(request):
     cat_list = Category.objects.all().order_by('id')[:10]
     pro_list = Product.objects.all().order_by('-price')[:5]
-    return render(request, 'storeapp/index.html', {'cat_list': cat_list, 'pro_list': pro_list, 'msg': msg})
+    return render(request, 'storeapp/index.html', {'cat_list': cat_list, 'pro_list': pro_list})
 
 
 def about(request):
