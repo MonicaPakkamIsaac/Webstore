8ca5bc6b78406f0164ddace335bb1df864533fba
rajocular <manivanr@uwindsor.ca>
2018-07-05 19:47:28 -0400

    EditProfileForm is created in forms.py

--- storeapp/forms.py
+++ storeapp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from storeapp.models import Order
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
 
 
 class OrderForm(forms.ModelForm):
@@ -12,3 +14,9 @@ class OrderForm(forms.ModelForm):
 class InterestForm(forms.Form):
     interested = forms.IntegerField(widget=forms.RadioSelect(choices=[('1', 'Yes'), ('0', 'No')]), required=True, initial=1)
     comments = forms.CharField(widget=forms.Textarea(), label="Additional Comments", required=False)
+
+
+class EditProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
