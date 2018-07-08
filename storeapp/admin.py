413ba6c2a3ebbccdb0a16f1db31cb298af3a63b6
monica <pakkami@uwindsor.ca>
2018-07-08 18:13:22 -0400

    Clientadmin is created in admin.py

--- storeapp/admin.py
+++ storeapp/admin.py
@@ -6,8 +6,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'available')
 
 
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'city', 'get_interests')
+
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
 admin.site.register(Order)
 admin.site.register(Product, ProductAdmin)
