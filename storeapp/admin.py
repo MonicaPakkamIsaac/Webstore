5dd1a67c7d3cbecc1e3845a7974bb67cc9df70ae
monica <pakkami@uwindsor.ca>
2018-07-08 18:11:35 -0400

    productadmin is created in admin.py

--- storeapp/admin.py
+++ storeapp/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Product, Category, Client, Order
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'available')
+
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Client)
 admin.site.register(Order)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
